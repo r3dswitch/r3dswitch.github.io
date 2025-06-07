3260f73c966812a65f424419584725391ecafa75
r3dswitch <iamsammyyo@gmail.com>
2025-06-07 15:49:12 +0200

    added robot emoji

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Soumya Mondal 🤖",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
